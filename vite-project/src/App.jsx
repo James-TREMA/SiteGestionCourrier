e696aad04acce982e82fcce58a2da87d8d5f8b78
James-TREMA <andrdu29@live.fr>
2024-01-17 19:43:44 +0000

    up

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from '../login';
-import InscriptionPage from '../inscription';
+import LoginPage from './login';
+import InscriptionPage from './inscription';
 
 const App = () => {
   return (
--- vite-project/src/inscription.jsx
+++ vite-project/src/inscription.jsx
@@ -9,12 +9,43 @@ const InscriptionPage = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // Logique pour envoyer les données au serveur
-    console.log({ firmName, firstName, lastName, email, phoneNumber, isAdmin });
-    // Vous pouvez utiliser fetch ou axios pour envoyer les données
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  // Préparation des données à envoyer
+  const userData = {
+    firm_name: firmName,
+    first_name: firstName,
+    last_name: lastName,
+    email: email,
+    phone_number: phoneNumber,
+    is_admin: isAdmin
   };
+
+  try {
+    const response = await fetch('http://51.83.69.229:3000/api/users/createUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // Gestion de la réponse réussie
+      console.log('Inscription réussie:', data);
+      // Redirection ou mise à jour de l'état ici
+    } else {
+      // Gestion des erreurs de réponse
+      console.error('Erreur lors de l\'inscription:', data.message);
+    }
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi de la requête:', error);
+  }
+};
+  
 
   return (
     <div>
