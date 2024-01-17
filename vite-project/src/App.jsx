0bfb2866aca3b949c33af78cef12996111b83e0a
James-TREMA <andrdu29@live.fr>
2024-01-17 19:03:36 +0000

    up

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../login.jsx"; // Importez votre composant Login
-// import DashboardClient from "./DashboardClient"; // Remplacez par votre composant de dashboard client
-// import DashboardGestionnaire from "./DashboardGestionnaire"; // Remplacez par votre composant de dashboard gestionnaire
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from '../login';
+import InscriptionPage from '../inscription';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        {/* <Route path="/client-dashboard" element={<DashboardClient />} />
-        <Route path="/manager-dashboard" element={<DashboardGestionnaire />} /> */}
-        {/* Ajoutez d'autres routes selon vos besoins */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/inscription" element={<InscriptionPage />} />
+        {/* Autres routes... */}
       </Routes>
     </Router>
   );
